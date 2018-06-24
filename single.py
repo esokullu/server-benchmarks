bbed6dd5c697c546d8ef3de9deb10f6e1015d4ca
Emre Sokullu <esokullu@gmail.com>
2018-06-24 03:34:42 -0700

    Add files via upload

---
+++ single.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+import time
+
+server_addr = '/tmp/uds_server.sock'
+
+duration = 1
+end = time.time() + duration
+msgs = 0
+
+print 'Receiving messages...'
+
+def printer():
+	return 'Hello there!'
+
+while time.time() < end:
+#    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+#    sock.connect(server_addr)
+#    data = sock.recv(32)
+    data = printer()
+    msgs += 1
+#    sock.close()
+
+print 'Received {} messages in {} second(s).'.format(msgs, duration)
